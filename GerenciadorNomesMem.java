92e47920c5075e31e074d87d777568a0b68ef6f1
Ricardo Andre Lopes Ikeda <89215809+RicardoALIkeda@users.noreply.github.com>
2024-09-09 09:33:27 -0300

    Update GerenciadorNomesMem.java

--- GerenciadorNomesMem.java
+++ GerenciadorNomesMem.java
@@ -1,3 +1,7 @@
+/*
+Diego Estevão Lopes de Queiroz - 10419038
+Ricardo André Lopes Ikeda - 10390256
+*/
 import java.util.*;    
     public class GerenciadorNomesMem implements GerenciadorNomes {
         private List<String> nomes = new ArrayList<String>();
