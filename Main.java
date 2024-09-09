b745f835503490a89d4df72dcb784a68e98ec9d1
Diego Estevão Lopes de Queiroz <deadzone8727@gmail.com>
2024-09-09 09:33:37 -0300

    nomes

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+/*
+Diego Estevão Lopes de Queiroz - 10419038
+Ricardo André Lopes Ikeda - 10390256
+*/
 public class Main {
   public static void main(String[] args) {
     GerenciadorNomes gNomes = new GerenciadorNomesMem();
